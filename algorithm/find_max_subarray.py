0a8092c76dccf6cefd72e2f94ab09a7af45af370
zhaoqiang_zq@126.com <zhaoqiang_zq@126.com>
2019-01-05 14:10:04 +0800

    穷举方式查找最大子数组

--- algorithm/find_max_subarray.py
+++ algorithm/find_max_subarray.py
@@ -2,21 +2,25 @@
 
 # 查找最大子数组
 
-
+# 穷举查找
 def find_max_subarray1(lists):
-    # 暴力查找
     max = 0
     start = 0
     end = 0
-    print(lists)
-    for i in range(0, len(lists)):
-        for j in range(i + 1, len(lists)):
+    length = len(lists)
+    for i in range(0, length):
+        for j in range(i + 1, length):
             sub_sum = sum(lists[i:j])
             if sub_sum > max:
                 max = sub_sum
                 start = i
                 end = j
     return list(lists[start:end])
+
+# 递归查找
+# 寻找一个基准，则最大子数组可能存在的位置，1， 基准左侧。2， 基准右侧。3， 跨基准(左侧最大后缀+右侧最大前缀)
+def find_max_subarray2(lists):
+    pass
 
 
 if __name__ == '__main__':
