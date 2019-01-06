1019c5fad1bec83bacbed96fe3a59299ff8ec3db
zhaoqiang_zq@126.com <zhaoqiang_zq@126.com>
2019-01-06 14:41:48 +0800

    递归求解一个最大子数组

--- algorithm/find_max_subarray.py
+++ algorithm/find_max_subarray.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# 查找最大子数组
+# 查找一个最大子数组
 
 # 穷举查找
 def find_max_subarray1(lists):
@@ -17,12 +17,21 @@ def find_max_subarray1(lists):
                 end = j
     return list(lists[start:end])
 
+
 # 递归查找
 # 寻找一个基准，则最大子数组可能存在的位置，1， 基准左侧。2， 基准右侧。3， 跨基准(左侧最大后缀+右侧最大前缀)
-def find_max_subarray2(lists):
-    pass
+def find_max_subarray2(lists, low, heigh):
+    if low == heigh:
+        return lists[low:heigh+1]
+    mid = (heigh+low)//2
+    left_sub = find_max_subarray2(lists, low, mid)
+    right_sub = find_max_subarray2(lists, mid+1, heigh)
+    return left_sub, right_sub
 
 
 if __name__ == '__main__':
     l = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
     print(find_max_subarray1(l))
+    l = [-2, 2, -3, 4, -1, 2, 1, -5, 3]
+    print(find_max_subarray1(l))
+    print(find_max_subarray2(l, 0, len(l)))
