eb544f6fc58f523ab3d9741c67e36a3fcdea2a5f
zhaoqiang_zq@126.com <zhaoqiang_zq@126.com>
2019-01-11 15:43:41 +0800

    数据结构：队列

---
+++ algorithm/queue.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+'''
+数据结构： 队列，线程不安全
+'''
+
+class Node(object):
+    def __init__(self, value):
+        self.next = None
+        self.prev = None
+        self.value = value
+
+class Queue(object):
+    def __init__(self):
+        pass
+
+    def en_queue(self, value):
+        pass
+
+    def de_queue(self):
+        pass
+
+    def __len__(self):
+        pass
+
+if __name__ == '__main__':
+    q = Queue()
