a22caf6ac078da1b6eb7d3c8d77c858b95f624a5
zhaoqiang_zq@126.com <zhaoqiang_zq@126.com>
2019-01-11 13:23:17 +0800

    数据结构栈

---
+++ algorithm/stack.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+'''
+栈结构的实现及操作
+'''
+
+class Node(object):
+    def __init__(self):
+        pass
+
+class Stack(object):
+
+    def __init__(self):
+        pass
+
+    def top(self):
+        '''
+        查看栈顶元素
+        :return:
+        '''
+
+    def pop(self):
+        '''
+        出栈
+        :return:
+        '''
+
+    def push(self, ele):
+        '''
+        入栈
+        :param ele:
+        :return:
+        '''
+
+if __name__ == '__main__':
+    stack = Stack()
